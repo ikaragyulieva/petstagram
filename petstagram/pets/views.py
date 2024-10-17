0cd1d64105eda49ffd7a8f7b976486720a50a15a
Ivelina Karagyulieva <ivelina.karagulieva@gmail.com>
2024-10-17 22:00:04 +0200

    Pet form functionalities - add pet, delete pet, edit pet added

--- petstagram/pets/views.py
+++ petstagram/pets/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from petstagram.pets.forms import PetForm, PetDeleteForm
 from petstagram.pets.models import Pet
 from petstagram.photos.models import Photo
 
@@ -7,7 +8,17 @@ from petstagram.photos.models import Photo
 # Create your views here.
 
 def add_pet(request):
-    return render(request, template_name='pets/pet-add-page.html')
+    form = PetForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('profile-details', pk=1)
+    else:
+        context = {
+            'form': form,
+        }
+
+    return render(request, template_name='pets/pet-add-page.html', context=context)
 
 
 def pet_details(request, username: str, pet_slug: str):
@@ -21,9 +32,32 @@ def pet_details(request, username: str, pet_slug: str):
     return render(request, template_name='pets/pet-details-page.html', context=context)
 
 
-def edit_pet(request):
-    return render(request, template_name='pets/pet-edit-page.html')
+def edit_pet(request, username, pet_slug):
+    pet = Pet.objects.get(slug=pet_slug)
+    if request.method == 'GET':
+        form = PetForm(instance=pet, initial=pet.__dict__)
+    else:
+        form = PetForm(request.POST, instance=pet)
+        if form.is_valid():
+            form.save()
+            return redirect('pet-details', username, pet_slug)
+
+    context = {
+        'form': form,
+    }
+    return render(request, template_name='pets/pet-edit-page.html', context=context)
 
 
-def delete_pet(request):
-    return render(request, template_name='pets/pet-delete-page.html')
+def delete_pet(request, username, pet_slug):
+    pet = Pet.objects.get(slug=pet_slug)
+
+    if request.method == 'POST':
+        pet.delete()
+        return redirect('profile-details', pk=1)
+
+    form = PetDeleteForm(instance=pet, initial=pet.__dict__)
+    context = {
+        'form': form,
+    }
+
+    return render(request, template_name='pets/pet-delete-page.html', context=context)
